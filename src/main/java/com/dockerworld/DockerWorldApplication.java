0dc78e5d74ad7b90e1b23ef81b0c1bc6335b2c6d
Razvan Stan <Razvan.Stan@4finance.com>
2019-11-08 15:13:50 +0200

    init-skeleton

---
+++ src/main/java/com/dockerworld/DockerWorldApplication.java
@@ -0,0 +1,13 @@
+package com.dockerworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dockerworld/DockerWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
